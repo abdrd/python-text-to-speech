66f68490c6e1d11cbefad61edbf54708b7be5664
Abidin Durdu <abidindrd@gmail.com>
2021-06-26 08:42:04 +0300

    refactor

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,3 +1,4 @@
+from os import read
 import pyttsx3
 import sys
 
@@ -13,45 +14,61 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-print(f"            {bcolors.HEADER} OPTIONS {bcolors.ENDC}")
-print(f"             -------                ")
-print(f" {bcolors.OKBLUE} -- SAY SOMETHING (s) {bcolors.ENDC} \n {bcolors.OKGREEN} -- READ FROM A TXT FILE (r) {bcolors.ENDC} \n ")
+def welcome():
+    print(f"            {bcolors.HEADER} OPTIONS {bcolors.ENDC}")
+    print(f"             -------                ")
+    print(f" {bcolors.OKBLUE} -- SAY SOMETHING (s) {bcolors.ENDC} \n {bcolors.OKGREEN} -- READ FROM A TXT FILE (r) {bcolors.ENDC} \n ")
 
-option = input(f"{bcolors.BOLD} r or s? {bcolors.ENDC} ")
+"""
+prompt the user and return the answers as a list
+[OPTION, READ_SPEED, VOLUME_INPUT, SAVE ("y" or "n") ]
+"""
+def prompt():    
+    option = input(f"{bcolors.BOLD} r or s? {bcolors.ENDC} ")
 
-read_speed = int(input(f" {bcolors.OKBLUE} Speed: {bcolors.ENDC} "))
-volume_input = float(input(f" {bcolors.OKBLUE} Volume : {bcolors.ENDC} "))
-save = input(f"{bcolors.OKBLUE} Save mp3? (y/n) ")
+    read_speed = int(input(f" {bcolors.OKBLUE} Speed: {bcolors.ENDC} "))
+    volume_input = float(input(f" {bcolors.OKBLUE} Volume : {bcolors.ENDC} "))
+    save = input(f"{bcolors.OKBLUE} Save mp3? (y/n) ")
 
-if save == 'y':
-    save_file_name = input(f"{bcolors.OKBLUE} save as ... ") + '.mp3'
-    
-engine = pyttsx3.init()
-
-engine.setProperty('rate', read_speed)
-engine.setProperty('volume', volume_input)
+    return [option, read_speed, volume_input, save]
 
 
-if option == 'r':
-    txt_file = input(f"file: ")
-    try:
-        f_handle = open(txt_file, 'r')
-        text = f_handle.read()
-        
+def init(option, read_speed, volume_input, save):
+    engine = pyttsx3.init()
+
+    engine.setProperty('rate', read_speed)
+    engine.setProperty('volume', volume_input)
+
+    if save == "y":
+        save_file_name = input(f"{bcolors.OKBLUE} save as ... ") + '.mp3'
+
+    if option == 'r':
+        txt_file = input(f"file: ")
+        try:
+            f_handle = open(txt_file, 'r')
+            text = f_handle.read()
+            
+            if save == 'y':                
+                engine.save_to_file(text, save_file_name)
+            engine.say(text)
+            engine.runAndWait()
+
+        except:
+            print(f" {bcolors.WARNING} invalid input {bcolors.ENDC} ")
+
+    elif option == 's':
+        to_say = input("SAY: ")
+
         if save == 'y':
-            engine.save_to_file(text, save_file_name)
+            engine.save_to_file(to_say, save_file_name)
 
-        engine.say(text)
+        engine.say(to_say)
         engine.runAndWait()
-    except:
-        print(f" {bcolors.WARNING} invalid input {bcolors.ENDC} ")
 
+# entry point to the app
+def App():
+    welcome()
+    choices = prompt()
+    init(choices[0], choices[1], choices[2], choices[3])
 
-elif option == 's':
-    to_say = input("SAY: ")
-
-    if save == 'y':
-        engine.save_to_file(to_say, save_file_name)
-
-    engine.say(to_say)
-    engine.runAndWait()
+App()
